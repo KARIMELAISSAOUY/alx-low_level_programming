e343efd1bb6ee7ba6deaf3d8ea0c0d048d9443d6
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-02-17 23:14:19 -0800

    Task5 ,Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,8 +6,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0,
- *        only using putchar and without char variables.
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
  * Return: Always 0.
  */
@@ -16,9 +15,9 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+		printf("%d", num);
 
-	putchar('\n');
+	printf("\n");
 
 	return (0);
 }
