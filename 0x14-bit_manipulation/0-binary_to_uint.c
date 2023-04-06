aa7befb8f39e0e422b13fab7eafab8b592c2d294
ABDELKARIM ELAISSAOUY <118484209+KARIMELAISSAOUY@users.noreply.github.com>
2023-04-06 10:48:54 +0000

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int result = 0;
+    int i;
+
+    if (b == NULL)
+        return 0;
+
+    for (i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] == '0')
+        {
+            result <<= 1;  // - equivalent to result = result * 2
+        }
+        else if (b[i] == '1')
+        {
+            result <<= 1;
+            result |= 1;  // - equivalent to result = result * 2 + 1
+        }
+        else
+        {
+            return 0;  // - invalid character in string
+        }
+    }
+
+    return result;
+}
