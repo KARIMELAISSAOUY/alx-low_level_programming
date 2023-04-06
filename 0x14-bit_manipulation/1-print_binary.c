628b967cf3e223ae78ee16c35d3afcb5ad3cac17
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-04-06 04:28:56 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
-/**
-*alx
-*/
 
 void print_binary(unsigned long int n)
 {
-if (n > 1)
-	print_binary(n >> 1);
-
-_putchar((n & 1) + '0');
+	if (n > 1)
+		print_binary(n >> 1);
+/**
+ *alx
+ */
+	_putchar((n & 1) + '0');
 }
