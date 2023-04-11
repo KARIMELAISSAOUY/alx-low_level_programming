45397177392ebf6f482507c708a21668f1000c65
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-04-10 20:52:36 -0700

    0x15-file_io alx

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,9 +2,9 @@
 
 /**
  * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
+ * @filename: A pointer to the name Of the file to create.
+ * @text_content: A pointers to a string to write to the file.
+ * @alx_karim
  * Return: If the function fails - -1.
  * Otherwise - 1.
  */
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,10 +1,10 @@
 #include"main.h"
 
 /**
- *append_text_to_file - appends text at the end of a file
- *@filename: A pointer to the name of the file.
- *@text_content: The string to add to the end of the file.
- *
+ *append_text_to_file -  appends text at the end of a file
+ *@filename: A Pointer To the name of the file.
+ *@text_content: The String To add to the end of the file.
+ *@alx_alx
  *Return: If the function fails or filename is NULL -1
  *	if the file does not exist the user lacks write permissions -1
  *	otherwise 1
